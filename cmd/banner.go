a827e18d1c361ba5db0c02ed6be358a801144525
vflame6 <vflame6x@gmail.com>
2026-04-10 07:14:36 +0500

    version string fix

--- cmd/banner.go
+++ cmd/banner.go
@@ -1,6 +1,9 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 // AUTHOR of the program
 const AUTHOR = "Maksim Radaev (@vflame6)"
@@ -10,6 +13,14 @@ var VERSION = "dev"
 
 // BANNER format string. It is used in PrintBanner function with VERSION
 var BANNER = "\n    __           __            \n   / /__  ____ _/ /_____  _____\n  / / _ \\/ __ `/ //_/ _ \\/ ___/\n / /  __/ /_/ / ,< /  __/ /    \n/_/\\___/\\__,_/_/|_|\\___/_/ %s\n\nMade by %s\n\n"
+
+func init() {
+	if VERSION == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			VERSION = info.Main.Version
+		}
+	}
+}
 
 // PrintBanner is a function to print program banner
 func PrintBanner() {
